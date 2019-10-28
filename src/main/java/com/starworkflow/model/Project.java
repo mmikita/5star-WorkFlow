f8fc8980ed68b9b20925914979aef8660b2f20f7
Michał Mikita <paster01@gmail.com>
2019-10-28 19:30:05 +0100

    add model layer

--- src/main/java/com/starworkflow/model/Project.java
+++ src/main/java/com/starworkflow/model/Project.java
@@ -1,26 +1,42 @@
 package com.starworkflow.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="project")
+@Table(name = "project")
 public class Project {
 	
 	 @GeneratedValue
 	 @Id private Long id;
-	 
 	 private String name;
+	 @OneToMany
+	 @JoinColumn(name = "project_id")
+	 private List<Status> statues;
 	 
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+	public List<Status> getStatues() {
+		return statues;
+	}
+
+	public void setStatues(List<Status> statues) {
+		this.statues = statues;
+	}
+
 	public String getName() {
 		return name;
 	}
