ea7a019283d8640e84283ae7227543798afd76cc
Michał Mikita <paster01@gmail.com>
2019-11-09 21:16:51 +0100

    really first commit

--- src/main/java/com/starworkflow/Application.java
+++ src/main/java/com/starworkflow/Application.java
@@ -1,9 +1,19 @@
-package com.workflow.com.workflow;
+package com.starworkflow;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+
+@EnableAutoConfiguration
+@ComponentScan(basePackages= "com.starworkflow")
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class Application {
 
 	public static void main(String[] args) {
