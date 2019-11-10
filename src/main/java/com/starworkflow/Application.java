2b27597c6cb432562d4559792bdaf7b3a1f01fbd
Michał Mikita <paster01@gmail.com>
2019-11-10 11:25:11 +0100

    add some randomh auth code

--- src/main/java/com/starworkflow/Application.java
+++ src/main/java/com/starworkflow/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableJpaRepositories
 @EnableTransactionManagement
+@EnableGlobalMethodSecurity(prePostEnabled=true,proxyTargetClass=true)
 public class Application {
 
 	public static void main(String[] args) {
